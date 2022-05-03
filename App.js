a0d63640dd42aa01eb70dfb5f8386208493372aa
ValentinCmbr <v.combier@lyon.ort.asso.fr>
2022-05-03 20:41:33 +0200

    delete et flatlist

--- App.js
+++ App.js
@@ -3,43 +3,70 @@ import { ImageBackground, StyleSheet, FlatList, Button, Text, View, TextInput, T
 import React, {useState} from "react";
 import {Touchable} from "react-native-web";
 
-
+const SampleGoals = [
+    {
+        id:'1',
+        title:'Faire les courses',
+    },
+    {
+        id:'2',
+        title:'Aller à la salle de sport 3 fois par semaine',
+    },
+    {
+        id:'3',
+        title:'Monter à plus de 5000m d altitude',
+    },
+    {
+        id:'4',
+        title:'Acheter mon premier appartement',
+    },
+    {
+        id:'5',
+        title:'Perdre 5 kgs',
+    },
+    {
+        id:'6',
+        title:'Gagner en productivité',
+    },
+    {
+        id:'7',
+        title:'Apprendre un nouveau langage',
+    },
+    {
+        id:'8',
+        title:'Faire une mission en freelance',
+    },
+    {
+        id:'9',
+        title:'Organiser un meetup autour de la tech',
+    },
+    {
+        id:'10',
+        title:'Faire un triathlon',
+    },
+]
 export default function App() {
-
-    const SampleGoals= [
-        'Faire les courses',
-        'Aller à la salle de sport 3 fois par semaine',
-        'Monter à plus de 5000m d altitude',
-        'Acheter mon premier appartement',
-        'Perdre 5 kgs',
-        'Gagner en productivité',
-        'Apprendre un nouveau langage',
-        'Faire une mission en freelance',
-        'Organiser un meetup autour de la tech',
-        'Faire un triathlon'
-    ];
-
-    const list = (List) => {
-        return List.map((Goal, index) => {
-            return (
-                <View><Text key={index}>{Goal}</Text><TouchableOpacity style={styles.button}
-                                                                       onPress={() =>SuppList(index)}><Text>Supprimer</Text></TouchableOpacity></View>);
-        });
-    }
 
     const [text, setText] = React.useState("");
     const[List, setListe] = React.useState(SampleGoals);
-    const onPress = () => setListe([...List,text]);
-    const SuppList = (key) => setListe(List.filter((text) =>text.key));
-
+    const onPress = () => setListe([...List,{id:List.length+1,title:text.toString()}]);
+    const DeleteItem = (id)=> {
+        const filtreListe = List.filter(item => item.id !== id);
+        setListe(filtreListe);
+    }
 
   return (
     <View style={styles.container}>
         <ImageBackground source={require('./assets/bckg.jpg')} resizeMode="cover" style={styles.image}>
-
       <Text style={styles.textUn}>test</Text>
       <Text>Open up <Text style={styles.textDeux}>App.js</Text> start working on your app!</Text>
-            {list(List)}
+            <FlatList data={List} renderItem={({item}) => <Text >{item.title}
+                <View style={styles.button}>
+                    <TouchableOpacity onPress={() => DeleteItem(item.id)}>
+                        <Text>Supprimer</Text>
+                    </TouchableOpacity>
+                </View>
+            </Text>}/>
         <View style={{flexDirection:'row'}}>
         <TextInput style={styles.input} placeholder="Votre texte ici" onChangeText={(value) => setText(value)}/>
         <TouchableOpacity style={styles.button} onPress={onPress}><Text>Add</Text></TouchableOpacity>
